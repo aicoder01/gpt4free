b30c24652373b09a78c17ce4f2fd7e27f3db743c
夏晴天 <137782658+aicoder01@users.noreply.github.com>
2023-10-08 17:26:52 +0800

    Update app.py

--- etc/interference/app.py
+++ etc/interference/app.py
@@ -157,7 +157,7 @@ def embeddings():
     }
 
 def main():
-    app.run(host='0.0.0.0', port=1337, debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True)
 
 if __name__ == '__main__':
     main()
